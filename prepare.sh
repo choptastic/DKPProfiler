ee9b9e9fd921faf45b5ec66f2955dbedc85562de
Jesse Gumm <sigmastar@gmail.com>
2011-12-15 23:51:37 -0600

    add comment to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# This will simply generate a DKPProfiler.zip file in your home directory.
+
 mkdir DKPProfiler
 cp *.* DKPProfiler
 zip -r -9 ~/DKPProfiler.zip DKPProfiler --exclude DKPProfiler/*~ DKPProfiler/*.zip
