f274962a86d4499cadd588f65ba7760fe6438311
Jesse Gumm <sigmastar@gmail.com>
2013-04-01 16:20:50 -0500

    Update for WoW 5.2

--- DKPProfiler.lua
+++ DKPProfiler.lua
@@ -4,7 +4,7 @@ DKPProfilerGuildBank = {};
 DKPProfilerBankTabTime = {};
 DKPProfilerAchCat = {};
 BankOpenedOnce = false;
-local DKPPVersion = "0.704 (2012-09-20)";
+local DKPPVersion = "0.705 (2013-04-01)";
 
 
 
